daa8bc69052a4974ffe6f914cfd222790ae72b51
Papuga <148044294+EasyDev-Papuga@users.noreply.github.com>
2024-07-29 20:42:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ let ifWin = false
 
 button.addEventListener('click', addScore)
 function addScore() {
-    localStorage.setItem('scoresv', scoreint)
     if(localStorage.getItem('scoresv')){
         scoreint = localStorage.getItem('scoresv')
     }
+    localStorage.setItem('scoresv', scoreint)
     league.textContent = `League: ${league_value}`
     if(scoreint == 20){
         button_img.setAttribute('src', `images/${imgsReplaced}.jpg`)
